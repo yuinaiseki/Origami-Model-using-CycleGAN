8721893cff15b13dbc74ce9b442318eb9f841625
yuinaiseki <isekiyui@grinnell.edu>
2025-10-23 21:17:33 -0700

    set up of framework

--- nst.py
+++ nst.py
@@ -1,1 +1,18 @@
-hello
+import torch
+from torchvision.models import vgg19
+
+# load pre-trained model and get weights
+vgg = vgg19(pretrained=True).features
+
+# load image and preprocess it for VGG19
+
+# convert image back to viewable from optimized tensor
+
+# feature extration 
+
+# gram matrix to capture style
+
+# style transfer
+
+# running the whole thing: 
+# if __name__ == "__main__"
